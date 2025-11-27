5f89051bf59004db72f88d692d0ff7872ffaa073
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-11-27 09:38:32 +0700

    Menambah interface container

---
+++ app/src/main/java/com/example/myroomsatu/repositori/ContainerApp.kt
@@ -0,0 +1,10 @@
+package com.example.myroomsatu.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.myroomsatu.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
